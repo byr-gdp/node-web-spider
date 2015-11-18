694f41675eb19cc25507f56aa73df202ce5782e5
byr-gdp <anthozoan77@gmail.com>
2015-11-19 00:56:06 +0800

    clean code for amazon-book.js

--- amazon-book.js
+++ amazon-book.js
@@ -1,7 +1,13 @@
+// 亚马逊 200 - 100 活动爬取活动书籍及价格
+// 活动地址详见 url
+
+var url = "http://www.amazon.cn/s/ref=sr_pg_2?rh=n%3A658390051%2Cn%3A%212146619051%2Cn%3A%212146621051%2Cn%3A1559230071%2Cp_6%3AA1AJ19PSB66TGU&page=2&bbn=1559230071&ie=UTF8&qid=1447603385"
+
 var http         = require("http");
 var cheerio      = require("cheerio");
-var iconv        = require('iconv-lite'); 
-var BufferHelper = require('bufferhelper');
+// urf8编码不需要
+// var iconv        = require('iconv-lite'); 
+// var BufferHelper = require('bufferhelper');
 
 function download(url, callback) {
     http.get(url, function(res) {
@@ -13,24 +19,20 @@ function download(url, callback) {
             // bufferHelper.concat(chunk);
         });
         res.on("end", function() {
-            // console.log('data: ' + data);
             callback(data);
-            // console.log(iconv.decode(bufferHelper.toBuffer(),'utf8'));
-            // callback(iconv.decode(bufferHelper.toBuffer(), 'utf8'));
+            // GBK 编码需要改为执行以下语句
+            // callback(iconv.decode(bufferHelper.toBuffer(), 'GBK'));
         });
     }).on("error", function() {
         callback(null);
     });
 }
 
-var url = "http://www.amazon.cn/s/ref=sr_pg_2?rh=n%3A658390051%2Cn%3A%212146619051%2Cn%3A%212146621051%2Cn%3A1559230071%2Cp_6%3AA1AJ19PSB66TGU&page=2&bbn=1559230071&ie=UTF8&qid=1447603385"
 
 download(url, function(data) {
     if (data) {
-        // console.log(data);
         var $ = cheerio.load(data);
 
-        // amazon
         $("li[class='s-result-item celwidget']").each(function(i, e) {
             var bookName = $(e).find("h2[class='a-size-medium a-color-null s-inline s-access-title a-text-normal']").text();
             var price = $(e).find("span[class='a-size-base a-color-price s-price a-text-bold']").text();
